76ac0c6468ecfe4721faf6d09ea7c13203049d5d
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-11 13:31:55 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,52 @@
 /* eslint-disable */
-const { withModuleFederationPlugin, shareAll } = require('@angular-architects/module-federation/webpack');
+// const { withModuleFederationPlugin, shareAll } = require('@angular-architects/module-federation/webpack');
+
+// module.exports = withModuleFederationPlugin({
+//   name: 'neosTemplate',
+//   filename: 'remoteEntry.js',
+//   exposes: {
+//     // './App': './src/bootstrap.ts',
+//     './public-api': './src/public-api.ts', // Exponemos todos los componentes
+//   },
+//   shared: shareAll({
+//     singleton: true,
+//     strictVersion: true,
+//     requiredVersion: 'auto',
+//   }),
+
+//   additionalShared:[],
+// });
+
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+const deps = require("./package.json").dependencies;
 
 module.exports = {
   output: {
-    // 👇 Aquí va fuera del plugin
-    publicPath: 'auto',
+    uniqueName: "neosTemplate",
+    publicPath: "auto"
   },
-
+  optimization: {
+    runtimeChunk: false
+  },
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/app/")
+    }
+  },
   plugins: [
-    withModuleFederationPlugin({
-      name: 'neosTemplate',
-      filename: 'remoteEntry.js',
+    new ModuleFederationPlugin({
+      name: "neosTemplate",
+      filename: "remoteEntry.js",
       exposes: {
-        
-        // './App': './src/bootstrap.ts'
-        './public-api': './src/public-api.ts',
+        "./public-api": "./src/public-api.ts"
       },
-      shared: shareAll({
-        singleton: true,
-        strictVersion: true,
-        requiredVersion: 'auto',
-      }),
-    }),
-  ],
+      shared: {
+        ...deps,
+        "@angular/core": { singleton: true, strictVersion: true, requiredVersion: deps["@angular/core"] },
+        "@angular/common": { singleton: true, strictVersion: true, requiredVersion: deps["@angular/common"] },
+        "@angular/router": { singleton: true, strictVersion: true, requiredVersion: deps["@angular/router"] }
+      }
+    })
+  ]
 };
