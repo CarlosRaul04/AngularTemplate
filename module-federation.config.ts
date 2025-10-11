e79c43db083410e599c4832eb50cba37aefeb0af
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-11 14:01:22 -0500

    Update module-federation.config.ts

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -6,7 +6,8 @@ const config: ModuleFederationConfig = {
   name: 'neosTemplate',
   exposes: {
     // './App': './src/app/remote-entry/bootstrap.ts',
-    './public-api': './src/public-api.ts',
+    // './public-api': './src/public-api.ts',
+    './RemoteWrapper': './src/app/presentation/features/remote-wrapper/remote-wrapper.component.ts',
   },
 };
 
