8b0a248860fb673d304bbfafb0c6c1273737f19a
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-11 14:14:13 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@
      strictVersion: true,
      requiredVersion: 'auto',
    }),
-
+   '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+   '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+   '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+   '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+   '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
  });
 
