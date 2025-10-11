64df31b76b784a8579dea798a648f00b956a34cd
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-11 13:46:02 -0500

    Update webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,25 +1,8 @@
-// /* eslint-disable */
-// const { merge } = require('webpack-merge');
-// const prodConfig = require('./webpack.config');
-// module.exports = merge(prodConfig, {
-//   mode: 'production',
-// });
-
 /* eslint-disable */
-/* eslint-disable */
-const { merge } = require("webpack-merge");
-const baseConfig = require("./webpack.config");
-
-module.exports = merge(baseConfig, {
-  mode: "production",
-  output: {
-    uniqueName: "neosTemplate",
-    publicPath: "auto",
-    clean: true,
-  },
-  optimization: {
-    runtimeChunk: false,
-    minimize: true,
-  },
+const { merge } = require('webpack-merge');
+const prodConfig = require('./webpack.config');
+module.exports = merge(prodConfig, {
+  mode: 'production',
 });
+
 
