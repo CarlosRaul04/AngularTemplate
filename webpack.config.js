75871150234986f7986153b8f3990c6104eb0411
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-11 13:20:56 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 /* eslint-disable */
 const { withModuleFederationPlugin, shareAll } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
-  name: 'neosTemplate',
-  filename: 'remoteEntry.js',
-  exposes: {
-    // './App': './src/bootstrap.ts',
-    './public-api': './src/public-api.ts', // Exponemos todos los componentes
-  },
-  shared: shareAll({
-    singleton: true,
-    strictVersion: true,
-    requiredVersion: 'auto',
-  }),
-
+module.exports = {
   output: {
+    // 👇 Aquí va fuera del plugin
     publicPath: 'auto',
   },
-});
+
+  plugins: [
+    withModuleFederationPlugin({
+      name: 'neosTemplate',
+      filename: 'remoteEntry.js',
+      exposes: {
+        
+        // './App': './src/bootstrap.ts'
+        './public-api': './src/public-api.ts',
+      },
+      shared: shareAll({
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto',
+      }),
+    }),
+  ],
+};
