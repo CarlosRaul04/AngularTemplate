6de1fdab8d8ddebdf4800dca3570d2962c65f2a4
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-11 14:19:57 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,18 @@
 /* eslint-disable */
-const {
-  withModuleFederationPlugin,
-  shareAll,
-} = require('@angular-architects/module-federation/webpack');
+ const { withModuleFederationPlugin, shareAll } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
-  name: 'neosTemplate',
-  filename: 'remoteEntry.js',
-  exposes: {
-    // './App': './src/bootstrap.ts',
+ module.exports = withModuleFederationPlugin({
+   name: 'neosTemplate',
+   filename: 'remoteEntry.js',
+   exposes: {
+     // './App': './src/bootstrap.ts',
     //  './public-api': './src/public-api.ts', // Exponemos todos los componentes
     './RemoteWrapper': './src/app/presentation/features/remote-wrapper/remote-wrapper.component.ts',
-  },
-  shared: {
-    ...shareAll({
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: 'auto',
-    }),
-    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-  },
-});
+   },
+   shared: shareAll({
+     singleton: true,
+     strictVersion: true,
+     requiredVersion: 'auto',
+   }),
+ });
+
