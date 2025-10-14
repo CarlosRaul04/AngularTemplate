200ff7cd1352d6a563991516b6b0ae11318a39f9
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 12:54:22 -0500

    Add files via upload

---
+++ src/app/presentation/features/navigation.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Injectable({ providedIn: 'root' })
+export class NavigationService {
+  private router = inject(Router);
+
+  goToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  goToWelcome() {
+    this.router.navigate(['layout', 'welcome']);
+  }
+}
