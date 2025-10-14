1a57bc1306f3cef3dc6c5963990b0b55b753e7d7
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 18:50:37 -0500

    Update entry.routes.ts

--- src/app/remote-entry/entry.routes.ts
+++ src/app/remote-entry/entry.routes.ts
@@ -1,11 +1,11 @@
-import { Routes } from '@angular/router';
-import { remoteProviders } from 'bootstrap.providers';
+// import { Routes } from '@angular/router';
+// import { remoteProviders } from 'bootstrap.providers';
 
-export const remoteRoutes: Routes = [
-  {
-    path: '',
-    providers: [...remoteProviders],
-    loadChildren: () =>
-      import('../app.routes').then((m) => m.routes), // tu definición original
-  },
-];
+// export const remoteRoutes: Routes = [
+//   {
+//     path: '',
+//     providers: [...remoteProviders],
+//     loadChildren: () =>
+//       import('../app.routes').then((m) => m.routes), // tu definición original
+//   },
+// ];
