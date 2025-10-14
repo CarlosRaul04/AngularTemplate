72f709467c3442e674ae17705544e99daebfbf80
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 11:30:31 -0500

    Update public-api.ts

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,11 +1,3 @@
 // Exponemos componentes
-export { LoginComponent } from './app/presentation/features/auth/login/login.component';
-export { ComponentsComponent } from './app/presentation/features/components/components.component';
-export { DocumentationComponent} from './app/presentation/features/documentation/documentation.component';
-export { LayoutComponent} from './app/presentation/features/layout/layout.component';
-export { WelcomeComponent } from './app/presentation/features/welcome/welcome.component';
-export { SettingsComponent } from './app/presentation/features/settings/settings.component';
 export { RemoteWrapperComponent } from './app/presentation/features/remote-wrapper/remote-wrapper.component';
 export * from './app/app.routes';
-export * from './app/app.config';
-
