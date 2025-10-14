5b419c8a364d055891bcbcee3a9b214a51aa8a20
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 15:48:50 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 /* eslint-disable */
  const { withModuleFederationPlugin, shareAll } = require('@angular-architects/module-federation/webpack');
 
-module.exports = {
-  output: {
-    uniqueName: 'neosTemplate',
-    publicPath: 'auto',
-    scriptType: 'text/javascript', // ⚡ Fuerza modo clásico (no ESM)
+module.exports = withModuleFederationPlugin({
+  name: 'neosTemplate',
+  filename: 'remoteEntry.js',
+  exposes: {
+    './Routes': './src/app/app.routes.ts',
   },
-
-  plugins: [
-    withModuleFederationPlugin({
-      name: 'neosTemplate',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './Routes': './src/app/app.routes.ts',
-      },
-      library: { type: 'var', name: 'neosTemplate' }, // ⚡ Expone window.neosTemplate
-      shared: shareAll({
-        singleton: true,
-        strictVersion: true,
-        requiredVersion: 'auto',
-      }),
-    }),
-  ],
-};
+  library: { type: 'var', name: 'neosTemplate' },
+  shared: shareAll({
+    singleton: true,
+    strictVersion: true,
+    requiredVersion: 'auto',
+  }),
+  // 👇 puedes añadir configuración global extra
+  additionalShared: [],
+  // 👇 esto se fusiona al nivel superior de webpack
+  extraOptions: {
+    output: {
+      uniqueName: 'neosTemplate',
+      publicPath: 'auto',
+      scriptType: 'text/javascript',
+    },
+  },
+});
