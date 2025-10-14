0a4518049998704a6efd4150e8c621936cb5c8a2
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 15:47:12 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 /* eslint-disable */
  const { withModuleFederationPlugin, shareAll } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
-  name: 'neosTemplate',
-  filename: 'remoteEntry.js',
-  exposes: {
-    './Routes': './src/app/app.routes.ts',
-  },
-  shared: shareAll({
-    singleton: true,
-    strictVersion: true,
-    requiredVersion: 'auto',
-  }),
-  // 👇 Ajustes clave para evitar el error de shareScope y 'init undefined'
-  library: { type: 'var', name: 'neosTemplate' },
+module.exports = {
   output: {
     uniqueName: 'neosTemplate',
     publicPath: 'auto',
-    scriptType: 'text/javascript', // ⚡️ fuerza modo clásico
+    scriptType: 'text/javascript', // ⚡ Fuerza modo clásico (no ESM)
   },
-});
+
+  plugins: [
+    withModuleFederationPlugin({
+      name: 'neosTemplate',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Routes': './src/app/app.routes.ts',
+      },
+      library: { type: 'var', name: 'neosTemplate' }, // ⚡ Expone window.neosTemplate
+      shared: shareAll({
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto',
+      }),
+    }),
+  ],
+};
