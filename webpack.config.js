7198ac0078930fdf06437bdfb46bcf7d29e90b65
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 16:55:55 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,14 @@
      // './App': './src/bootstrap.ts',
     //  './public-api': './src/public-api.ts', // Exponemos todos los componentes
     // './RemoteWrapper': './src/app/presentation/features/remote-wrapper/remote-wrapper.component.ts',
-    './Routes': './src/app/app.routes.ts',
+    // './Routes': './src/app/app.routes.ts',
+    './RoutesModule': './src/app/remote-routes.module.ts',
    },
    shared: shareAll({
      singleton: true,
      strictVersion: true,
      requiredVersion: 'auto',
-   }),
+   })
+   ,
  });
 
