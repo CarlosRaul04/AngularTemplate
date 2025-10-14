3e93f5a92ce5b5fd939a0343c5a70fbcaa0be296
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 11:30:15 -0500

    Update module-federation.config.ts

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -7,8 +7,8 @@ const config: ModuleFederationConfig = {
   exposes: {
     // './App': './src/app/remote-entry/bootstrap.ts',
     // './public-api': './src/public-api.ts',
-    './RemoteWrapper': './src/app/presentation/features/remote-wrapper/remote-wrapper.component.ts',
-    './public-api': './src/public-api.ts',
+    // './RemoteWrapper': './src/app/presentation/features/remote-wrapper/remote-wrapper.component.ts',
+    './Routes': './src/app/app.routes.ts',
   },
 };
 
