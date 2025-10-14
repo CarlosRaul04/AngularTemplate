eb27dd32f72cab8a8a5d77b0591ea6949b45ffcb
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 17:33:45 -0500

    Update bootstrap.ts

--- src/app/remote-entry/bootstrap.ts
+++ src/app/remote-entry/bootstrap.ts
@@ -1,17 +1,3 @@
-// // src/app/remote-entry/bootstrap.ts
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { App } from '@app/app';
-// import { appConfig } from '../app.config'; // si tienes providers globales
 
-// // Inicializador que arranca la app remotamente
-// export async function mountNeosApp(element: HTMLElement) {
-//   // Crea dinámicamente el elemento raíz
-//   const root = document.createElement('div');
-//   root.id = 'neos-root';
-//   element.appendChild(root);
-
-//   // Bootstrap de la app
-//   await bootstrapApplication(App, appConfig);
-
-//   return root;
-// }
+import { routes } from '../app.routes';
+export const remoteRoutes = routes;
