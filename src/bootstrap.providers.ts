fcce33c4971472f0563e76cb12aed8ce575ed13d
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 11:42:44 -0500

    Add files via upload

---
+++ src/bootstrap.providers.ts
@@ -0,0 +1,15 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { AuthRepository } from '@domain/repositories/auth.repository';
+import { AuthRepositoryImpl } from '@infra/auth/auth.repository.impl';
+import { LoginUseCase } from '@domain/usecases/login.usecase';
+import { LoginUseCaseImpl } from '@app/application/auth/login.usecase.impl';
+import { LogoutUseCase } from '@domain/usecases/logout.usecase';
+import { LogoutUseCaseImpl } from './app/application/auth/logout.usecae.impl'; 
+
+export const remoteProviders = [
+  provideHttpClient(withFetch()),
+
+  { provide: AuthRepository, useClass: AuthRepositoryImpl },
+  { provide: LoginUseCase, useClass: LoginUseCaseImpl },
+  { provide: LogoutUseCase, useClass: LogoutUseCaseImpl },
+];
