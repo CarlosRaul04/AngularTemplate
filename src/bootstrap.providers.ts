a325d9a235865ccaae6df7346c288f1bed363a4d
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-14 19:10:51 -0500

    Update bootstrap.providers.ts

--- src/bootstrap.providers.ts
+++ src/bootstrap.providers.ts
@@ -1,4 +1,4 @@
-// import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AuthRepository } from '@domain/repositories/auth.repository';
 import { AuthRepositoryImpl } from '@infra/auth/auth.repository.impl';
 import { LoginUseCase } from '@domain/usecases/login.usecase';
@@ -9,10 +9,9 @@ import { AuthFacade } from '@app/presentation/facades/auth.facade';
 import { ConfirmDialogService } from '@app/shared/services/confirm-dialog.service';
 import { RemoteLocationStrategy } from '@app/remote-location-strategy';
 import { LocationStrategy } from '@angular/common';
-// import { provideRouter, withRouterConfig } from '@angular/router';
-// import { routes } from '@app/app.routes';
 
 export const remoteProviders = [
+  provideHttpClient(withFetch()),
   { provide: AuthRepository, useClass: AuthRepositoryImpl },
   { provide: LoginUseCase, useClass: LoginUseCaseImpl },
   { provide: LogoutUseCase, useClass: LogoutUseCaseImpl },
