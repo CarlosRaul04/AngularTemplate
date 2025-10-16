2b8b58c9d049d7702e747aa7a9e00d1af797b962
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-15 19:50:45 -0500

    Update bootstrap.providers.ts

--- src/bootstrap.providers.ts
+++ src/bootstrap.providers.ts
@@ -13,7 +13,7 @@ import { APP_BASE_HREF } from '@angular/common';
 
 export const remoteProviders = [
   provideHttpClient(withFetch()),
-  { provide: APP_BASE_HREF, useValue: '/menu/neos' },
+  { provide: APP_BASE_HREF, useValue: '/neos' },
   { provide: AuthRepository, useClass: AuthRepositoryImpl },
   { provide: LoginUseCase, useClass: LoginUseCaseImpl },
   { provide: LogoutUseCase, useClass: LogoutUseCaseImpl },
@@ -21,5 +21,6 @@ export const remoteProviders = [
   AuthFacade,
   ConfirmDialogService,
 ];
+
 
 
