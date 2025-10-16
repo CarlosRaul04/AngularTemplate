c53a3f11d6b1f8df2f4cf127ecd42da593336ebb
Carlos Ramirez <Carlos.ramirez.cuentas04@gmail.com>
2025-10-16 17:16:30 -0500

    Update bootstrap.providers.ts

--- src/bootstrap.providers.ts
+++ src/bootstrap.providers.ts
@@ -7,8 +7,6 @@ import { LogoutUseCase } from '@domain/usecases/logout.usecase';
 import { LogoutUseCaseImpl } from './app/application/auth/logout.usecae.impl'; 
 import { AuthFacade } from '@app/presentation/facades/auth.facade';
 import { ConfirmDialogService } from '@app/shared/services/confirm-dialog.service';
-import { RemoteLocationStrategy } from '@app/remote-location-strategy';
-import { LocationStrategy } from '@angular/common';
 import { APP_BASE_HREF } from '@angular/common';
 
 export const remoteProviders = [
@@ -17,10 +15,10 @@ export const remoteProviders = [
   { provide: AuthRepository, useClass: AuthRepositoryImpl },
   { provide: LoginUseCase, useClass: LoginUseCaseImpl },
   { provide: LogoutUseCase, useClass: LogoutUseCaseImpl },
-  { provide: LocationStrategy, useClass: RemoteLocationStrategy },
   AuthFacade,
   ConfirmDialogService,
 ];
+
 
 
 
